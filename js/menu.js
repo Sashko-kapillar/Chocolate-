281c082ceca100ab1506a009fa7a971a72bc247a
sashko-kapillar <kapillarua@gmail.com>
2024-11-04 21:16:46 +0200

    add anchor

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuToggle = document.getElementById("toggle-open");
-  const sideMenu = document.getElementById("sideMenu");
-  const body = document.body;
+const menuToggle = document.getElementById("toggle-open");
+const sideMenu = document.getElementById("sideMenu");
+const menuLinks = document.querySelectorAll(".menu-link");
+const body = document.body;
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Додаємо обробник для відкриття/закриття меню через чекбокс
   menuToggle.addEventListener("change", function () {
     if (menuToggle.checked) {
       sideMenu.classList.add("active");
@@ -11,5 +13,15 @@ document.addEventListener("DOMContentLoaded", function () {
       sideMenu.classList.remove("active");
       body.style.overflowY = "auto";
     }
+  });
+
+  // Додаємо обробник для закриття меню при кліку на кожне посилання
+  menuLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      // Знімаємо позначку з чекбокса, щоб закрити меню
+      menuToggle.checked = false;
+      sideMenu.classList.remove("active");
+      body.style.overflowY = "auto";
+    });
   });
 });
